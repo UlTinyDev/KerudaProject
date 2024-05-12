2f76637558c9f2100498cc0361ea19e6aa764e83
zsigsza <bercplays@gmail.com>
2024-05-12 20:47:29 +0200

    removed search

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,8 +15,5 @@
         <img src="keruda_logo.png" alt="Logo" class="h-12 mt-2 mb-2">
     </div>
     <div class="navbar-end">
-      <button class="btn btn-ghost btn-circle">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
-      </button>
     </div>
   </div>
