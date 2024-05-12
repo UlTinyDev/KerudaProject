1d6912e2ce3a8841ece5c660c341e0f94da75238
zsigsza <bercplays@gmail.com>
2024-05-12 21:18:58 +0200

    login form, and .vscode folder

---
+++ src/lib/components/FormInput.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let type = 'text'
+    export let placeholder = ''
+    export let cornerPlaceholder = ''
+</script>
+
+<label class="w-full max-w-xs my-2">
+    <div class="label">
+      <span class="label-text">{cornerPlaceholder}</span>
+    </div>
+    
+    <label class="input input-bordered flex items-center gap-2">
+        Name
+        <input type="text" class="grow" placeholder="Daisy" />
+    </label>
+</label>
---
+++ src/routes/app/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+  import FormInput from '$lib/components/FormInput.svelte';
+
+    /** @type {import('./$types').PageData} */
+    export let data;
+</script>
+
+
+<div class="h-lvh w-96 bg-base-300 flex items-center">
+    <form class="w-full flex flex-col items-center">
+        <p class="text-2xl m-6">Login</p>
+
+        
+        <FormInput placeholder="mari néni" cornerPlaceholder="Felhasználó név" />
+        <FormInput placeholder="mari néni" cornerPlaceholder="Felhasználó név" />
+    </form>
+</div>
