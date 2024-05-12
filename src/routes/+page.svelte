7c1d72527bc7e740d0c97d9458fbeca606786f89
zsigsza <bercplays@gmail.com>
2024-05-12 20:41:48 +0200

    test pull

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
           <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
         </div>
         <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-          <li><a>Homepage</a></li>
+          <li><a>Homepage among us</a></li>
           <li><a>Portfolio</a></li>
           <li><a>About</a></li>
         </ul>
