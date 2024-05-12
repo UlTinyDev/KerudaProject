4e28a8c0edb3c8be5414e047183731cc4a101108
zsigsza <bercplays@gmail.com>
2024-05-12 21:50:03 +0200

    page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,12 @@
-<<<<<<< HEAD
-<div class="hero bg-base-200">
-  <div class="hero-content h-12 flex-col lg:flex-row">
-=======
 <div class="hero mt-12">
   <div class="hero-content flex-col lg:flex-row">
     <!-- svelte-ignore a11y-missing-attribute -->
->>>>>>> 53dfdffb5ccb286e8d263f2f8eab43195d02170e
     <img
       src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
       class="max-w-sm rounded-lg shadow-2xl"
     />
     <div>
-      <h1 class="text-5xl font-bold">Box Office News!a</h1>
+      <h1 class="text-5xl font-bold">Box Office News!</h1>
       <p class="py-6">
         Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
         excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a
