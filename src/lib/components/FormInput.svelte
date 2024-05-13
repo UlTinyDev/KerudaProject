e16ea31c79fa8b9bba82d87775f6fa5513c25ae1
zsigsza <bercplays@gmail.com>
2024-05-13 15:49:52 +0200

    updated login page

--- src/lib/components/FormInput.svelte
+++ src/lib/components/FormInput.svelte
@@ -5,13 +5,15 @@
   export let icon = "";
 </script>
 
-<label class="w-full max-w-xs my-2">
-  <div class="label">
-    <span class="label-text">{cornerPlaceholder}</span>
-  </div>
+<div class="mb-3">
+  <label class="w-full max-w-xs">
+    <div class="label">
+      <span class="label-text">{cornerPlaceholder}</span>
+    </div>
 
-  <label class="input input-bordered flex items-center gap-2">
-    <p class="material-symbols-rounded text-md">{icon}</p>
-    <input {type} class="grow" {placeholder} />
+    <label class="input input-bordered flex items-center gap-2">
+      <p class="material-symbols-rounded text-md">{icon}</p>
+      <input {type} class="grow" {placeholder} />
+    </label>
   </label>
-</label>
+</div>
--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -2,14 +2,21 @@
   import FormInput from "$lib/components/FormInput.svelte";
 </script>
 
-<!-- TODO: Make the rest of the login page -->
 <div
   class="h-full w-full grid place-items-center sm:fix-login sm:bg-base-300 pt-36 sm:pt-0 sm:w-96"
 >
   <form class="text-center">
-    <p class="text-xl">Login</p>
-    <FormInput></FormInput>
-    <FormInput></FormInput>
-    <FormInput></FormInput>
+    <p class="text-xl mb-4">Login</p>
+    <FormInput
+      cornerPlaceholder="Email"
+      icon="email"
+      placeholder="amongus@yahoo.com"
+    />
+    <FormInput
+      cornerPlaceholder="Jelszó"
+      icon="key"
+      placeholder="123456"
+      type="password"
+    />
   </form>
 </div>
