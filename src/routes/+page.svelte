37642d475b89f96b9cde98abb100a4ed635806f2
Csibor <m1csibor6552@proton.me>
2024-05-14 22:44:25 +0200

    cuccosok laptop

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,13 @@
-<div class="hero bg-base-200"></div>
-<div class="hero-content h-12 flex-col lg:flex-row"></div>
 <div class="hero mt-12">
-  <div class="hero-content flex-col lg:flex-row">
-    <!-- svelte-ignore a11y-missing-attribute -->
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
-      class="max-w-sm rounded-lg shadow-2xl"
-    />
-    <div>
-      <h1 class="text-5xl font-bold mb-6">A torrentelés jövőbeli portálja.</h1>
-      <button class="btn btn-primary">Kezdjünk neki!</button>
+  <div class="hero-content text-center">
+    <div class="max-w-md">
+      <h1 class="text-5xl font-bold">Hello there</h1>
+      <p class="py-6">
+        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+        excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a
+        id nisi.
+      </p>
+      <button class="btn btn-primary">Get Started</button>
     </div>
   </div>
 </div>
